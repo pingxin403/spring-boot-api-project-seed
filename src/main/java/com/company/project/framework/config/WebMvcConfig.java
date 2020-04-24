f4d97ffc3bd78124575b7f74ea0fd8366d35bedb
hanyunpeng <m13839441583@163.com>
2020-04-24 14:24:35 +0800

    修改FastJson结合Swagger2配置SnakeCase错误bug

--- src/main/java/com/company/project/framework/config/WebMvcConfig.java
+++ src/main/java/com/company/project/framework/config/WebMvcConfig.java
@@ -1,19 +1,28 @@
 package com.company.project.framework.config;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.PropertyNamingStrategy;
+import com.alibaba.fastjson.serializer.NameFilter;
+import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.env.EnvironmentEndpoint;
+import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.UiConfiguration;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -43,18 +52,45 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+
+
+
         FastJsonConfig config = new FastJsonConfig();
         config.setSerializerFeatures(SerializerFeature.WriteMapNullValue);
         //保留空的字段
         //SerializerFeature.WriteNullStringAsEmpty,//String null -> ""
         //SerializerFeature.WriteNullNumberAsZero//Number null -> 0
-        // 按需配置，更多参考FastJson文档哈
-        config.setSerializerFeatures(SerializerFeature.WriteEnumUsingToString);
 
+        //序列化配置
+        SerializeConfig serializeConfig = new SerializeConfig();
+
+        //配置Swagger
+        NameFilter nameFilter = (object, name, value) -> name;
+        serializeConfig.addFilter(UiConfiguration.class, nameFilter);
+        serializeConfig.addFilter(SwaggerResource.class, nameFilter);
+        serializeConfig.addFilter(MetricsEndpoint.MetricResponse.class, nameFilter);
+        serializeConfig.addFilter(EnvironmentEndpoint.EnvironmentDescriptor.class, nameFilter);
+
+        serializeConfig.setPropertyNamingStrategy(PropertyNamingStrategy.SnakeCase);
+
+        config.setSerializeConfig(serializeConfig);
+
+        // 按需配置，更多参考FastJson文档哈
+        config.setDateFormat(JSON.DEFFAULT_DATE_FORMAT);
+        config.setSerializerFeatures(SerializerFeature.PrettyFormat);
 
         converter.setFastJsonConfig(config);
         converter.setDefaultCharset(Charset.forName("UTF-8"));
-        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON_UTF8));
+
+        //支持的类型
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON);
+        fastMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+        fastMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+        fastMediaTypes.add(MediaType.TEXT_HTML);
+        fastMediaTypes.add(new MediaType("application", "vnd.spring-boot.actuator.v2+json"));
+        converter.setSupportedMediaTypes(fastMediaTypes);
+
         converters.add(converter);
     }
 
